54c667c1b6dfe4818689ca775c52dae5bc75b6d3
Dirk007 <dirk@faustbande.de>
2024-11-12 20:00:05 +0100

    Do not repeat slow dying containers

--- src/clogger/finder.rs
+++ src/clogger/finder.rs
@@ -1,6 +1,7 @@
 use super::helper;
 
 pub struct FindQuery {
+    last: Option<String>,
     pub name: Option<String>,
     pub image: Option<String>,
 }
@@ -9,12 +10,16 @@ impl FindQuery {
     pub fn new(find_text: impl Into<String>) -> Self {
         let text = find_text.into().to_lowercase();
         FindQuery {
+            last: None,
             name: Some(text.clone()),
             image: Some(text),
         }
     }
 
     pub fn is_match(&self, container: &docker_api::models::ContainerSummary) -> bool {
+        if self.last == container.id {
+            return false;
+        }
         let cname = helper::extract_name(container).to_lowercase();
         if let Some(name) = &self.name {
             if cname.contains(name) {
